759f690a09243c3d5ac7e09bc476222079192f43
YoussefAbaas <76699316+YoussefAbaas@users.noreply.github.com>
2022-10-15 15:46:35 +0200

    Rte_Task5

---
+++ Rte_Task5/IoHwAbs_Template.c
@@ -0,0 +1,174 @@
+/**
+ *
+ * \file IoHwAbs_Template.c
+ * \brief Rte Component Template for AUTOSAR SWC: IoHwAbs
+ *
+ * \author Sprints AUTOSAR Authoring Tool (SAAT) v1.0.2
+ * Generated on 10/4/2022 09:07 PM
+ *
+ * For any inquiries: hassan.m.farahat@gmail.com
+ *
+ */
+
+#include "Rte_IoHwAbs.h"
+
+
+/**
+ *
+ * Runnable IoHwAbs_EcuGet_Slide
+ *
+ * Triggered By:
+ *  - OperationInvokedEventImpl.OIE_ppIoGetSlide_IOGet
+ *
+ */
+
+void IoHwAbs_EcuGet_Slide (IoPositionSensorReadingType* position)
+{
+	Std_ReturnType status;
+
+}
+
+
+/**
+ *
+ * Runnable IoHwAbs_EcuGet_Height
+ *
+ * Triggered By:
+ *  - OperationInvokedEventImpl.OIE_ppIoGetHeight_IOGet
+ *
+ */
+
+void IoHwAbs_EcuGet_Height (IoPositionSensorReadingType* position)
+{
+	Std_ReturnType status;
+
+}
+
+
+/**
+ *
+ * Runnable IoHwAbs_EcuGet_Incline
+ *
+ * Triggered By:
+ *  - OperationInvokedEventImpl.OIE_ppIoGetIncline_IOGet
+ *
+ */
+
+void IoHwAbs_EcuGet_Incline (IoPositionSensorReadingType* position)
+{
+	Std_ReturnType status;
+
+}
+
+
+/**
+ *
+ * Runnable IoHwAbs_EcuGet_Weight
+ *
+ * Triggered By:
+ *  - OperationInvokedEventImpl.OIE_ppIoGetWeight_IOGet
+ *
+ */
+
+void IoHwAbs_EcuGet_Weight (IoWeightSensorReadingType* weight)
+{
+	Std_ReturnType status;
+
+}
+
+
+/**
+ *
+ * Runnable IoHwAbs_EcuSetForward_Height
+ *
+ * Triggered By:
+ *  - OperationInvokedEventImpl.OIE_ppIoSetHeight_IOSetForward
+ *
+ */
+
+void IoHwAbs_EcuSetForward_Height (void)
+{
+	Std_ReturnType status;
+
+}
+
+
+/**
+ *
+ * Runnable IoHwAbs_EcuSetForward_Incline
+ *
+ * Triggered By:
+ *  - OperationInvokedEventImpl.OIE_ppIoSetSlide_IOSetForward
+ *
+ */
+
+void IoHwAbs_EcuSetForward_Incline (void)
+{
+	Std_ReturnType status;
+
+}
+
+
+/**
+ *
+ * Runnable IoHwAbs_EcuSetForward_Slide
+ *
+ * Triggered By:
+ *  - OperationInvokedEventImpl.OIE_ppIoSetSlide_IOSetForward
+ *
+ */
+
+void IoHwAbs_EcuSetForward_Slide (void)
+{
+	Std_ReturnType status;
+
+}
+
+
+/**
+ *
+ * Runnable IoHwAbs_EcuSetReverse_Height
+ *
+ * Triggered By:
+ *  - OperationInvokedEventImpl.OIE_ppIoSetHeight_IOSetReverse
+ *
+ */
+
+void IoHwAbs_EcuSetReverse_Height (void)
+{
+	Std_ReturnType status;
+
+}
+
+
+/**
+ *
+ * Runnable IoHwAbs_EcuSetReverse_Incline
+ *
+ * Triggered By:
+ *  - OperationInvokedEventImpl.OIE_ppIoSetIncline_IOSetReverse
+ *
+ */
+
+void IoHwAbs_EcuSetReverse_Incline (void)
+{
+	Std_ReturnType status;
+
+}
+
+
+/**
+ *
+ * Runnable IoHwAbs_EcuSetReverse_Slide
+ *
+ * Triggered By:
+ *  - OperationInvokedEventImpl.OIE_ppIoSetSlide_IOSetReverse
+ *
+ */
+
+void IoHwAbs_EcuSetReverse_Slide (void)
+{
+	Std_ReturnType status;
+
+}
+
